1696c22dbbf5bb0d987142f5970839a67df96ee8
Salar <mhmdi.salar@gmail.com>
2022-01-02 01:43:57 +0330

    Update blanckterm.sh

--- blanckterm.sh
+++ blanckterm.sh
@@ -9,14 +9,14 @@ declare -r DIR=`SRCDIR "${BASH_SOURCE[0]}"`;
 cd $DIR;
 ##########################################################################
 ## VARIABLES
+declare -r etcDir="$PREFIX/etc";
+declare -r vncDir="$HOME/.vnc";
 declare -r aPp=$0;
 declare -r cfgDir="$DIR/config"; ! [[ -d $cfgDir ]] && mkdir $cfgDir;
 declare -r statValDir="$cfgDir/statval"; ! [[ -d $statValDir ]] && mkdir $statValDir;
 declare -r isUpg="$statValDir/isUpg";
 declare -r isCfgE="$statValDir/isCfgE";
 declare -r isInsE="$statValDir/isInsE";
-declare -r etcDir="$PREFIX/etc";
-declare -r vncDir="$HOME/.vnc"; ! [[ -d $vncDir ]] && mkdir $vncDir;
 ############################################################################
 ############################################################################
 #############################################################################
@@ -49,22 +49,19 @@ insEsse () {
 apt-get install x11-repo -y && apt-get update -y && apt-get upgrade -y && apt-get install proot -y && apt-get install xorg-fonts-100dpi xorg-iceauth xorg-luit xorg-server xorg-server-static xorg-twm xorg-util-macros xorg-xcalc xorg-xclock xorg-xdpyinfo xorg-xev xorg-xhost xorg-xlsfonts xorg-xmessage xorg-xprop xorg-xrandr xorg-xrdb xorg-xsetroot xorg-xwininfo xorgproto libx11-static libxss -y && apt-get install termux-x11 termux-auth termux-exec termux-create-package termux-services termux-elf-cleaner termux-apt-repo termux-api termimage ncurses-utils ncurses-ui-libs-static lf asciinema -y && apt-get install picom putty -y && apt-get install qt5-qtx11extras -y && apt-get install tigervnc -y && apt-get install xfce4 -y && apt-get install xfce4-goodies xfce4-wavelan-plugin-static xfce4-timer-plugin-static xfce4-screenshooter-static xfce4-places-plugin xfce4-places-plugin-static xfce4-panel-static xfce4-notifyd-static xfce4-notes-plugin-static xfce4-mailwatch-plugin-static xfce4-eyes-plugin-static xfce4-dict-static xfce4-datetime-plugin-static xfce4-clipman-plugin-static xfce4-calculator-plugin-static xfce-theme-manager libxfce4util-static libxfce4ui-static -y && apt-get install wget curl git man bc nmap xfconf xfconf-static -y && touch $isInsE;
 }
 cfgEsse () {
+	echo 'export DISPLAY="localhost:0"' >> "$etcDir/bash.bashrc";
 	echo 'declare -i vncc=0; for vncl in `vncserver -list`; do vncc+=1; done;' >> "$etcDir/bash.bashrc";
-	echo '[[ $vncc -le 8 ]] && vncserver -name blanckxterm; vncserver -list;' >> "$etcDir/bash.bashrc";
+	echo '[[ $vncc -le 8 ]] && vncserver :13 -name blanckxterm -geometry 1920x1080 -localhost; vncserver -list;' >> "$etcDir/bash.bashrc";
 	touch $isCfgE;
 }
 vncSetup () {
 # VNC X Startup Config
 local -r vncXS="$vncDir/xstartup";
-local -r vncCfg="$vncDir/config";
-echo '#!/usr/bin/env bash' > $vncXS;
-echo 'xfce4-session &' >> $vncXS;
-echo "localhost" > $vncCfg;
-echo "geometry=2333x1080" >> $vncCfg;
-echo 'export DISPLAY=localhost:0' >> "$etcDir/bash.bashrc";
 # VNC Start
-vncserver -name blanckxterm;
+vncserver :13 -name blanckxterm -geometry 1920x1080 -localhost;
 vncserver -list;
+echo '#!/usr/bin/env bash' > $vncXS;
+echo "xfce4-session &" >> $vncXS;
 }
 ###
 MAIN () {
