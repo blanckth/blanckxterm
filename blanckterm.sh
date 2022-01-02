64e234d44c9578ffcd91e43edba005322c7993f1
Salar <mhmdi.salar@gmail.com>
2022-01-02 14:23:48 +0330

    Update blanckterm.sh

--- blanckterm.sh
+++ blanckterm.sh
@@ -46,7 +46,24 @@ echo '++ Run VNC : vncserver [-name <blanckxterm>]' >> "$motdd";
 echo "==========================" >> "$motdd";
 }
 insEsse () {
-apt-get install x11-repo -y && apt-get update -y && apt-get upgrade -y && apt-get install proot -y && apt-get install xorg-fonts-100dpi xorg-iceauth xorg-luit xorg-server xorg-server-static xorg-twm xorg-util-macros xorg-xcalc xorg-xclock xorg-xdpyinfo xorg-xev xorg-xhost xorg-xlsfonts xorg-xmessage xorg-xprop xorg-xrandr xorg-xrdb xorg-xsetroot xorg-xwininfo xorgproto libx11-static libxss -y && apt-get install termux-x11 termux-auth termux-exec termux-create-package termux-services termux-elf-cleaner termux-apt-repo termux-api termimage ncurses-utils ncurses-ui-libs-static lf asciinema -y && apt-get install picom putty -y && apt-get install qt5-qtx11extras -y && apt-get install tigervnc -y && apt-get install xfce4 -y && apt-get install xfce4-goodies xfce4-wavelan-plugin-static xfce4-timer-plugin-static xfce4-screenshooter-static xfce4-places-plugin xfce4-places-plugin-static xfce4-panel-static xfce4-notifyd-static xfce4-notes-plugin-static xfce4-mailwatch-plugin-static xfce4-eyes-plugin-static xfce4-dict-static xfce4-datetime-plugin-static xfce4-clipman-plugin-static xfce4-calculator-plugin-static xfce-theme-manager libxfce4util-static libxfce4ui-static -y && apt-get install wget curl git man bc nmap xfconf xfconf-static -y && touch $isInsE;
+apt-get install x11-repo -y && apt-get update -y && apt-get upgrade -y && \
+apt-get install proot -y && \
+apt-get install xorg-fonts-100dpi xorg-iceauth xorg-luit xorg-server xorg-server-static \
+xorg-twm xorg-util-macros xorg-xcalc xorg-xclock xorg-xdpyinfo xorg-xev xorg-xhost xorg-xlsfonts \
+xorg-xmessage xorg-xprop xorg-xrandr xorg-xrdb xorg-xsetroot xorg-xwininfo xorgproto \
+libx11-static libxss -y && \
+apt-get install termux-x11 termux-auth termux-exec termux-create-package termux-services \
+termux-elf-cleaner termux-apt-repo termux-api termimage ncurses-utils ncurses-ui-libs-static lf asciinema -y && \
+apt-get install picom putty -y && \
+apt-get install qt5-qtx11extras -y && \
+apt-get install tigervnc -y && \
+apt-get install xfce4 -y && apt-get install xfce4-goodies \
+xfce4-wavelan-plugin-static xfce4-timer-plugin-static xfce4-screenshooter-static \
+xfce4-places-plugin xfce4-places-plugin-static xfce4-panel-static xfce4-notifyd-static \
+xfce4-notes-plugin-static xfce4-mailwatch-plugin-static xfce4-eyes-plugin-static xfce4-dict-static \
+xfce4-datetime-plugin-static xfce4-clipman-plugin-static xfce4-calculator-plugin-static xfce-theme-manager \
+libxfce4util-static libxfce4ui-static -y && \
+apt-get install wget curl git man bc nmap xfconf xfconf-static -y && touch $isInsE;
 }
 cfgEsse () {
 	echo 'export DISPLAY="localhost:0"' >> "$etcDir/bash.bashrc";
@@ -73,7 +90,9 @@ ChColors MENUQ;
 TypingS "Salar Muhammadi!" 0.0123;
 SGR RSGR;
 echo;
-pingGo; echo; ! [[ "$?" -eq 0 ]] && ChColors FAILED && TypingS "Check Internet Connection and again Enter $aPp" 0.0123 && SGR RSGR && exit 111;
+pingGo;
+! [[ "$?" -eq 0 ]] && ChColors FAILED && TypingS "Check Internet Connection and again Enter $aPp" 0.0123 && SGR RSGR && exit 111;
+echo;
 local -i isUp;
 ! [[ -f $isUpg ]] && fupg && ! [[ -f $isUpg ]] && ChColors FAILED && TypingS "Check System Upgrade Error and again Enter $aPp" 0.0123 && SGR RSGR && exit 222;
 ! [[ -f $isInsE ]] && insEsse && ! [[ -f $isInsE ]] && ChColors FAILED && TypingS "Check Package install Error and again Enter $aPp" 0.0123 && SGR RSGR && exit 333;
@@ -82,7 +101,7 @@ declare -i vncc=0; for vncl in `vncserver -list`; do vncc+=1; done; [[ $vncc -le
 ! [[ -f $isCfgE ]] && cfgEsse;
 termux-setup-storage;
 ChColors SUCCESS;
-TypingS "Configuration is Successful! Please Restart the session by CTRL + D and open the app again, Then you can use Desktop Env with a VNC-Viewer app like AVNC if you mind!" 0.0333;
+TypingS "Configuration is Successful! Please Restart the session by CTRL + D and open the app again, Then you can use Desktop Env with a VNC-Viewer app like AVNC if you mind!" "0.0333";
 SGR RSGR;
 }
 MAIN;
